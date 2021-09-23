065ea4c9ea8e4000d214ba55542f5cf5560f2e5e
julianlondono123 <julianlondonocol5@gmail.com>
2021-09-23 18:41:25 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.Math;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 julian londono
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner (System.in);
+        String remove;
+        String[] names = {"Chris Julio", "John James", "Rich Dave", "Keith Williams", "Lionel Messi"};
+        String new_names [] = new String[names.length-1];
+        System.out.println("There are 5 employees");
+
+        for (int i =0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+
+        System.out.println("Enter an employee name to remove");
+        remove = scan.nextLine();
+
+        for (int i=0; i< names.length; i++){
+            String test = names[i];
+            if (remove.equals(test)){
+                System.arraycopy(names, 0, new_names, 0, i);
+                System.arraycopy(names,  i+ 1, new_names, i, names.length - i - 1);
+            }
+        }
+
+        System.out.println("There are " + new_names.length + " employees");
+        for (int i =0; i< new_names.length; i++){
+            System.out.println(new_names[i]);
+        }
+    }
+}
